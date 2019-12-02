a41c924221322c61231503db440a8c78e7aa9976
Natalie Adams <natalie._.adams@hotmail.com>
2019-12-02 12:45:48 +1100

    Add files via upload

---
+++ Basic Operators Javascript.js
@@ -0,0 +1,14 @@
+var health = 50;
+health = health + 10; // store 60 
+health = health % 50 ; // store 10
+
+// heatlh % 2 == 0
+health = health ** 2; // 100
+
+// +=, -=, *=, /=
+health -= 20: health = health - 20
+
+// ++, --
+health ++; //81
+
+// health = (health * 2) + (health / 5);
